1a1bb112993728bc9c5a3a7c062b8d6f69ba0c6d
Евгений Зайнутдинов <evgeniy.zaynutdinov@gmail.com>
2024-12-22 18:04:37 +0300

    TASK-1 editing customer

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -24,7 +24,7 @@ class CustomerController extends AbstractController
     public function show(EntityManagerInterface $entityManager, int $id): Response
     {
         $customer = $entityManager->getRepository(Customer::class)->find($id);
-        return $this->render('customer.html.twig', ['customer' => $customer]);
+        return $this->render('customer.html.twig', ['customer' => $customer, 'states' => StateCode::CODES_WITH_NAMES]);
     }
 
     #[Route('/customer/create', name: 'customer_create_form', methods: ['GET'])]
@@ -56,6 +56,34 @@ class CustomerController extends AbstractController
             ->setSsn($data->get('ssn'))
             ->setAge($data->get('age'))
             ->setPhoneNumber($data->get('phoneNumber'));
+        $entityManager->persist($customer);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('customer_get', ['id' => $customer->getId()]);
+    }
+
+    #[Route('/customer/edit/{id}', name: 'customer_edit', methods: ['POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $data = $request->getPayload();
+        $customer = $entityManager->getRepository(Customer::class)->find($id);
+        $address = $customer->getAddress();
+        $address
+            ->setState($data->get('state'))
+            ->setCity($data->get('city'))
+            ->setZip($data->get('zip'))
+            ->setAddress($data->get('address'));
+
+        $customer
+            ->setFirstName($data->get('firstName'))
+            ->setLastName($data->get('lastName'))
+            ->setEmail($data->get('email'))
+            ->setFico($data->get('fico'))
+            ->setSsn($data->get('ssn'))
+            ->setAge($data->get('age'))
+            ->setPhoneNumber($data->get('phoneNumber'));
+
+        $entityManager->persist($address);
         $entityManager->persist($customer);
         $entityManager->flush();
 
